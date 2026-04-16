2ae6315a4a21abe32731608c38b1dca0c3bd1c09
damianeub <damianeub@gmail.com>
2026-04-16 16:15:59 -0700

    Updated Sample Data

--- database/seeding/sample_data.sql
+++ database/seeding/sample_data.sql
@@ -1,12 +1,10 @@
 
---Insert values into the college table. Zip codes are not accurate; just tests.
 
 INSERT INTO college_info VALUES
     ("University of Nevada Las Vegas", 12345),
     ("University of Nevada Reno", 98765),
     ("College of Southern Nevada", 12346);
 
---Insert a few sample degree types into the degree information table.
 INSERT INTO degree_info VALUES
     ("Computer Science", "Bachelor's of Science", "Engineering"),
     ("Cardiology", "Master's of Medicine", "Medical"),
@@ -16,18 +14,16 @@ INSERT INTO degree_info VALUES
     ("Engineering", "Bachelor's of Science", "Engineering"),
     ("Mathematics", "Bachelor's of Science", "Mathematics");
 
---Career and job link
 
 INSERT INTO job_info VALUES
-    (1, "Las Vegas", "ACME", 60000, "Data Scientist", "Full time",  NULL),
-    (2, "Las Vegas", "ACME", 50000, "Graphic Designer", "Full time",  NULL),
-    (3, "Las Vegas", "MSTS", 80000, "Security Officer", "Full time",  NULL),
-    (4, "Las Vegas", "MSTS", 45000, "Associate in Information Technology", "Part time",  NULL),
-    (5, "Livermore", "LLNL", 110000, "DevOps Engineer", "Full time",  NULL),
-    (6, "Livermore", "LLNL", 100000, "Nuclear Engineer", "Full time",  NULL),
-    (7, "North Las Vegas", "MSTS", 50000, "Pipefitter", "Full time", "5 years");
+    (1, "Las Vegas", "ACME", 60000, "Data Scientist", "Full time",  NULL, NULL, NULL),
+    (2, "Las Vegas", "ACME", 50000, "Graphic Designer", "Full time",  NULL,NULL, NULL),
+    (3, "Las Vegas", "MSTS", 80000, "Security Officer", "Full time",  NULL, NULL, NULL),
+    (4, "Las Vegas", "MSTS", 45000, "Associate in Information Technology", "Part time",  NULL, NULL, NULL),
+    (5, "Livermore", "LLNL", 110000, "DevOps Engineer", "Full time",  NULL, NULL, NULL),
+    (6, "Livermore", "LLNL", 100000, "Nuclear Engineer", "Full time",  NULL, NULL, NULL),
+    (7, "North Las Vegas", "MSTS", 50000, "Pipefitter", "Full time", "5 years", NULL, NULL);
 
---Insert sample values into the career table, with some null values to test if it is allowed in the schema (it should be.)
 INSERT INTO career_info VALUES
     ("Plumbing",  NULL,"Trade school"),
     ("Computer Science",  NULL,"Bachelor's Degree"),
@@ -35,7 +31,6 @@ INSERT INTO career_info VALUES
     ("Security",  NULL, "Certification"),
     ("Graphic Design",  NULL, "Bachelor's Degree");
 
---Table that allows multiple jobs to connect to a career
 INSERT INTO career_job_junction VALUES
     ("Plumbing", 7),
     ("Computer Science", 1),
@@ -46,21 +41,18 @@ INSERT INTO career_job_junction VALUES
     ("Graphic Design", 2);
 
 
---Course information
 INSERT INTO course_info VALUES
     (1, "CS460", "Compilers", "Computer Science", "University of Nevada Las Vegas"),
     (2, "CS135", "Introduction to Programming", "Computer Science", "College of Southern Nevada"),
     (3, "MATH182", "Calculus 2", "Mathematics", "University of Nevada Reno"),
     (4, "EGG101", "Introduction to Engineering Ethics", "Engineering", "University of Nevada Las Vegas");
 
---User information
 INSERT INTO user_info VALUES
-    (1, "password123", "example@gmail.com", NULL,  NULL,  NULL,  NULL, "Computer Science",  NULL, "High School", 2026,  NULL, "University of Nevada Las Vegas", "Computer Science"),
-    (2, "example321", "email@yahoo.com", 89149, "7021234567",  NULL,  NULL, "Graphic Design",  NULL, "Bachlor's of Art", 2022,  NULL, "College of Southern Nevada", "Graphic Design"),
-    (3, "anotherpass", "something@myspace.com", 12345, "0987654321",  NULL,  NULL, "Nuclear Engineering",  NULL, "Master's Degree", 2011,  NULL, "University of Nevada Las Vegas", "Nuclear Engineering"),
-    (4, "placeholder", "johndoe@gmail.com", NULL,  NULL,  NULL,  NULL, "Plumbing",  NULL, "Trade School", 2020,  NULL, NULL, "Plumbing");
+    (1, "password123", "example@gmail.com", NULL,  NULL,  NULL,  NULL, NULL, "Computer Science",  NULL, "High School", 2026,  NULL, "University of Nevada Las Vegas", "Computer Science", NULL, TRUE, 10),
+    (2, "example321", "email@yahoo.com", 89149, "7021234567",  NULL,  NULL, NULL, "Graphic Design",  NULL, "Bachlor's of Art", 2022,  NULL, "College of Southern Nevada", "Graphic Design", NULL, TRUE, 12),
+    (3, "anotherpass", "something@myspace.com", 12345, "0987654321",  NULL,  NULL, NULL, "Nuclear Engineering",  NULL, "Master's Degree", 2011,  NULL, "University of Nevada Las Vegas", "Nuclear Engineering", NULL, FALSE, 11),
+    (4, "placeholder", "johndoe@gmail.com", NULL,  NULL,  NULL,  NULL, "Plumbing",  NULL, NULL, "Trade School", 2020,  NULL, NULL, "Plumbing", NULL, FALSE, 27);
 
---Junction table that allows users to have multiple courses assigned to them
 INSERT INTO user_course_junction VALUES
     (1, 1),
     (1, 2),
@@ -68,7 +60,6 @@ INSERT INTO user_course_junction VALUES
     (3, 3),
     (3, 4);
 
---Junction table that allows users to have multiple different degrees assigned to them.
 INSERT INTO user_degree_junction VALUES
     (1, "Computer Science"),
     (2, "Graphic Design"),
